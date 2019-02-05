01f4a5c0ea8b0ca47e47f80c954c19316e163630
Marcus Hawthorne <emailmarcus8@gmail.comname>
2019-02-05 11:22:03 -0600

    Implemented a user view

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -27,4 +27,7 @@ export class UsersService {
     return this.http.get<User>(this.url);
   }
 
+  getUser(id: string): Observable<User> {
+    return this.http.get<User>(`${this.url}/${id}`);
+  }
 }
